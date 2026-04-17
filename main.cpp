e54f129a22fdcd02b9ffebf78d40c0bbdb011496
박민수 <minsu@bagminsuui-MacBookPro.local>
2026-04-17 16:12:01 +0900

    main.cpp 수정

--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
-#include "Movie.h"
-#include <iostream>
-#include <vector>
+#include "MovieSystem.h"
 
 int main() {
-    std::vector<Movie> movies;
-
-
-    movies.push_back(Movie(1, "Inception", "Sci-Fi", 2010));
-    movies.push_back(Movie(2,"Parasite", "Thriller", 2019));
-    movies.push_back(Movie(3, "Interstellar", "Sci-FI", 2014));
-    movies.push_back(Movie(4, "Arrival", "Sci-FI", 2016));
-    
-    movies[0].addRating(4.8);
-    movies[0].addRating(4.5);
-    movies[1].addRating(5.0);
-
-    for(const Movie& m : movies){
-        m.display();
-    }
-
-    std::cout << "총 " << movies.size() << "편" << std::endl;
-
+    MovieSystem app; 
+    app.run();       
     return 0;
 }
